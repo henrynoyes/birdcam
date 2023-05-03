2847c303cd3b85fe935cefc90fe0c568593b4e23
Henry Noyes <noyes.he@northeastern.edu>
2023-05-03 14:57:54 -0400

    edit main script for local development

--- camera/camera.py
+++ camera/camera.py
@@ -1,38 +1,57 @@
 import time
 import io
-from libcamera import Transform, controls
-from picamera2 import Picamera2
 from datetime import datetime
+import cv2 
+# from libcamera import Transform, controls
+# from picamera2 import Picamera2
 
 
-class VideoCamera(object):
-    def __init__(self, file_type=".jpg", photo_string="stream_photo"):
-        tune = Picamera2.load_tuning_file("/usr/share/libcamera/ipa/raspberrypi/imx219_noir.json")
-        self.vs = Picamera2(tuning=tune)
-        self.capture_config = self.vs.create_still_configuration()
-        self.vs.configure(self.vs.create_preview_configuration({"size": (800, 600)}, transform=Transform(hflip=True, vflip=True)))
-        self.vs.start()
-        self.file_type = file_type
-        self.photo_string = photo_string
-        time.sleep(2.0)
+# class VideoCamera(object):
+#     def __init__(self, file_type=".jpg", photo_string="stream_photo"):
+#         tune = Picamera2.load_tuning_file("/usr/share/libcamera/ipa/raspberrypi/imx219_noir.json")
+#         self.vs = Picamera2(tuning=tune)
+#         self.capture_config = self.vs.create_still_configuration()
+#         self.vs.configure(self.vs.create_preview_configuration({"size": (800, 600)}, transform=Transform(hflip=True, vflip=True)))
+#         self.vs.start()
+#         self.file_type = file_type
+#         self.photo_string = photo_string
+#         time.sleep(2.0)
 
-    def __del__(self):
-        print("STOP")
+#     def __del__(self):
+#         print("STOP")
 
-    def get_frame(self):
-        jpeg = io.BytesIO()
-        try:
-            self.vs.capture_file(jpeg, format='jpeg')
-        except RuntimeError:
-            jpeg = self.previous_frame
-        self.previous_frame = jpeg
-        return jpeg.getbuffer()
+#     def get_frame(self):
+#         jpeg = io.BytesIO()
+#         try:
+#             self.vs.capture_file(jpeg, format='jpeg')
+#         except RuntimeError:
+#             jpeg = self.previous_frame
+#         self.previous_frame = jpeg
+#         return jpeg.getbuffer()
 
-    # Take a photo, called by camera button
-    def take_picture(self):
-        today_date = datetime.now().strftime("%m%d%Y-%H%M%S")
-        print(self.vs.capture_file(today_date+self.file_type))
+#     # Take a photo, called by camera button
+#     def take_picture(self):
+#         today_date = datetime.now().strftime("%m%d%Y-%H%M%S")
+#         print(self.vs.capture_file(today_date+self.file_type))
         
-    def clip(self):
-        date = datetime.now().strftime("%m%d%Y-%H%M%S")
+#     def clip(self):
+#         date = datetime.now().strftime("%m%d%Y-%H%M%S")
+
+def det_motion(img, mog):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    fgmask = mog.apply(gray)
+    
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    fgmask = cv2.dilate(fgmask, kernel, iterations=1)
+    
+    contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    for contour in contours:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        
+        x, y, w, h = cv2.boundingRect(contour)
+        # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        print(x,y,w,h)
         
--- main.py
+++ main.py
@@ -1,33 +1,47 @@
+import sys
+sys.path.append('./camera/')
 from flask import Flask, render_template, Response, request, send_from_directory
-from camera import VideoCamera
+from camera import det_motion
 import os
+import cv2
+from io import BytesIO
+import numpy as np
 
 # Vertically flip video stream
-pi_camera = VideoCamera()
+# pi_camerea = VideoCamera()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./static/')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def gen(camera):
+def gen():
     # Establish video stream
+    mog = cv2.createBackgroundSubtractorMOG2()
     while True:
-        frame = camera.get_frame()
+        # frame = camera.get_frame()
+        with open('static/living_room.jpg','rb') as im:
+            jpg = BytesIO(im.read())
+        frame = jpg.getbuffer()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+        img = cv2.imdecode(np.frombuffer(jpg.read(), np.uint8), 1)
+        det_motion(img, mog)
+        # push phone
+
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(pi_camera),
+    return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # Take a photo when pressing camera button
 @app.route('/picture')
 def take_picture():
-    pi_camera.take_picture()
+    # pi_camera.take_picture()
     return "None"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=False)
+    # app.run(host='0.0.0.0', port=8000, debug=False)
+    app.run(host='127.0.0.1', port=8000, debug=False)
