f96781439a3b89b5b5fc5731a02c440faa1d40a2
Henry Noyes <noyes.he@northeastern.edu>
2023-05-09 21:08:56 -0400

    lightweight modec visualization

--- camera/h_motion_light.py
+++ camera/h_motion_light.py
@@ -16,16 +16,28 @@ import numpy as np
 #         # buffer = det_motion(curr_img, prev_img, buffer)
 #         prev_img = curr_img
 
-def det_motion(curr_img, prev_img, buffer):
+def det_motion(prev_img, curr_img, buffer):
     curr_grey = cv2.cvtColor(curr_img, cv2.COLOR_BGR2GRAY)
     prev_grey = cv2.cvtColor(prev_img, cv2.COLOR_BGR2GRAY)
-
+    curr_img = cv2.resize(curr_img, (640,360))
+    prev_img = cv2.resize(prev_img, (640,360))
     mse = np.square(np.subtract(curr_grey, prev_grey)).mean()
-    if mse > 10 and buffer > 120:
-        print(mse, buffer)
-        buffer = 0
-    buffer += 1
-    return buffer
+    sub = np.subtract(curr_grey, prev_grey)
+    sub = cv2.resize(sub, (800, 450))
+    # if mse > 10 and buffer > 20:
+    #     print(mse, buffer)
+    #     cv2.imshow('sub', sub)
+    #     cv2.waitKey(1)
+    #     buffer = 0
+    # buffer += 1
+    # return buffer
+    cv2.imshow('Frame T', curr_img)
+    cv2.imshow('Frame T-1', prev_img)
+    cv2.moveWindow('Frame T', 0, 320)
+    cv2.imshow('Frame Subtraction', sub)
+    cv2.moveWindow('Frame Subtraction', 640, 100)
+    cv2.waitKey(500)
+
 
     # diff = cv2.absdiff(curr_grey, prev_grey)
     # diff = cv2.dilate(diff, np.ones((5,5)), 1)
@@ -48,12 +60,12 @@ if __name__ == "__main__":
 
     cap = cv2.VideoCapture(0)
     prev_img = None
-    buffer = 121
+    buffer = 0
     while True:
         ret, curr_img = cap.read()
         if type(prev_img) != np.ndarray:
             prev_img = curr_img
-        buffer = det_motion(prev_img, curr_img, buffer)
+        det_motion(prev_img, curr_img, buffer)
         prev_img = curr_img
 
     cap.release()
