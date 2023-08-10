2ee10642856fd7d7cb9e094a2890e2f15c2af314
Henry Noyes <noyes.he@northeastern.edu>
2023-08-10 16:29:19 -0400

    clean up master and update readme

--- gen-gif.sh
+++ gen-gif.sh
@@ -1,3 +1,6 @@
+# Usage: ./gen-gif.sh <INPUT> <START TIME> <LENGTH> <FPS> <WIDTH> <OUTPUT>
+# ex: ./gen-gif.sh input.mp4 10 5 30 640 output.gif
+
 pal="[0:v] fps=$4,scale=$5:-1,split [a][b];[a] palettegen [p];[b][p] paletteuse"
 
 ffmpeg -i "$1" -ss "$2" -t "$3" -filter_complex "$pal" "$6"
