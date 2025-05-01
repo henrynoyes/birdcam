b3b8f488b862e845085d073b46a81146a5773baf
Henry Noyes <noyes.he@northeastern.edu>
2025-05-01 19:48:20 -0400

    remove logging

--- stream_youtube.sh
+++ stream_youtube.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source "$HOME/.profile"
-libcamera-vid --lens-position 8.0 -t 0 --nopreview -v 0 --mode 1920:1080 --framerate 30 --bitrate 2500000 --inline -o - -g 60 | ffmpeg -f lavfi -i anullsrc -thread_queue_size 1024 -use_wallclock_as_timestamps 1 -i pipe:0 -c:v copy -f flv -v verbose "rtmp://a.rtmp.youtube.com/live2/${YT_KEY}" >> &
+libcamera-vid --lens-position 8.0 -t 0 --nopreview -v 0 --mode 1920:1080 --framerate 30 --bitrate 2500000 --inline -o - -g 60 | ffmpeg -f lavfi -i anullsrc -thread_queue_size 1024 -use_wallclock_as_timestamps 1 -i pipe:0 -c:v copy -f flv -v verbose "rtmp://a.rtmp.youtube.com/live2/${YT_KEY}" &
